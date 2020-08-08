52951fc8c0aac16baa265fbc98469d0ec0ca8659
Colin Poirier <cppoirier92@hotmail.com>
2020-08-07 22:38:32 -0400

    system dark mode splash screen

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=c:\Users\Colin Poirier\Documents\Flutter\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Colin Poirier\Documents\Loan Calculator\loan_calc_dev"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=c:\Users\Colin Poirier\Documents\Flutter\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=6"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
